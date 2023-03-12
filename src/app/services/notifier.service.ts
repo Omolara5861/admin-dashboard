6b2d801bc67703de94a47b7f87db3a55cb008909
Omolara5861 <debbiegterra@gmail.com>
2023-03-12 15:52:01 +0100

    docs: :bulb: Add comment to notification service

--- src/app/services/notifier.service.ts
+++ src/app/services/notifier.service.ts
@@ -8,6 +8,12 @@ import { NotifierComponent } from '../shared/notifier/notifier.component';
 export class NotifierService {
 constructor(private snackBar: MatSnackBar) { }
 
+/**
+ *This method uses snackBar object of MatSnackBar to open a notification from a custom component called NotifierComponent. The notification is customized by passing some configuration options to openFromComponent method
+ * @param displayMessage is the message to display in the notification
+ * @param buttonText is the text to display on the action button of the notification
+ * @param messageType is the type of notification to display. It can be either an error or success notification
+ */
   showNotification(displayMessage: string, buttonText: string, messageType: 'error' | 'success') {
     this.snackBar.openFromComponent(NotifierComponent, {
       data: {
