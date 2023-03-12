cb5141124115a3a89d855b6797840729c3823011
Omolara5861 <debbiegterra@gmail.com>
2023-03-12 16:32:37 +0100

    docs: :bulb: Add comment to root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,23 +5,24 @@ import { delay, filter } from 'rxjs/operators';
 import { NavigationEnd, Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+/** The class AppComponent is decorated with the UntilDestroy decorator, which automatically unsubscribes from observables when the component is destroyed. */
 @UntilDestroy()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+  /** Get a reference to the MatSidenav component in the template */
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
 
   constructor(private observer: BreakpointObserver, private router: Router) {}
 
-  ngOnInit(): void {
-
-  }
-
   ngAfterViewInit() {
+    /**
+     * Observes the viewport width using BreakpointObserver. If the viewport width is less than or equal to 800px, it sets the MatSidenav mode to "over" and closes it. Otherwise, it sets the mode to "side" and opens it. This is to provide a responsive layout for the application.
+     */
     this.observer
       .observe(['(max-width: 800px)'])
       .pipe(delay(1), untilDestroyed(this))
@@ -35,6 +36,9 @@ export class AppComponent implements OnInit {
         }
       });
 
+      /**
+       * Listens to NavigationEnd events from the Router and checks if the MatSidenav mode is "over". If it is, it closes the MatSidenav. This is to ensure that the navigation menu is hidden on mobile devices when navigating to a new page.
+       */
     this.router.events
       .pipe(
         untilDestroyed(this),
@@ -46,5 +50,4 @@ export class AppComponent implements OnInit {
         }
       });
   }
-  // this.name = localStorage.getItem('name')!;
 }
