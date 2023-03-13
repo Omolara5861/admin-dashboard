320d8b3a9ddfd029e143babd0b47835b7c1dd299
Omolara5861 <debbiegterra@gmail.com>
2023-03-13 11:25:58 +0100

    feat: :sparkles: Add todos selectors to wrapper

--- src/app/pages/todos-wrapper/todos-wrapper.component.ts
+++ src/app/pages/todos-wrapper/todos-wrapper.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todos-wrapper',
-  template: ``
+  template: `
+  <app-todo></app-todo>
+  <app-api-todos></app-api-todos>
+  `
 })
 export class TodosWrapperComponent {
 
