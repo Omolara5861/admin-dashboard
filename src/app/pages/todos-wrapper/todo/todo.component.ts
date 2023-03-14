714f3de44fa4153c6bf6a15a3e7ca292c4afdf2f
Omolara5861 <debbiegterra@gmail.com>
2023-03-14 15:14:10 +0100

    feat: :necktie: Create logic for todo functionality

--- src/app/pages/todos-wrapper/todo/todo.component.ts
+++ src/app/pages/todos-wrapper/todo/todo.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { BehaviorSubject } from 'rxjs';
 import { Todo, BtnState } from 'src/app/model/project';
 import { TodosService } from 'src/app/services/todo.service';
+import { TodoModalComponent } from './edit-modal/todo-modal.component';
 
 
 
@@ -11,7 +13,50 @@ import { TodosService } from 'src/app/services/todo.service';
   styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  todoObj : Todo = new Todo();
+  todoArr : Todo[] = [];
+
+  addTodoValue : string = '';
+  editTodoValue : string = '';
+
+  constructor(private todoService : TodosService, public dialog: MatDialog) { }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.editTodoValue = '';
+    this.addTodoValue = '';
+    this.todoObj = new Todo();
+    this.todoArr = [];
+    this.getAllTodo();
+  }
+  getAllTodo() {
+    this.todoService.getAllTodo().subscribe(res => {
+      this.todoArr = res;
+    }, err => {
+      alert("Unable to get list of todos");
+    });
+  }
+
+  addTodo() {
+    this.todoObj.todo_name = this.addTodoValue;
+    this.todoService.addTodo(this.todoObj).subscribe(res => {
+      this.ngOnInit();
+      this.addTodoValue = '';
+    }, err => {
+      alert(err);
+    })
+  }
+
+  openTodo() {
+    this.dialog.open(TodoModalComponent, {
+      width: "40%"
+    });
+  }
+
+  deleteTodo(todo : Todo) {
+    this.todoService.deleteTodo(todo).subscribe(res => {
+      this.ngOnInit();
+    }, err => {
+      alert("Failed to delete todo");
+    });
   }
 }
