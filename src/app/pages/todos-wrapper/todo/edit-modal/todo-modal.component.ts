c8b9f0709f4c8a83decbf7ebe2c58117a1d393b5
Omolara5861 <debbiegterra@gmail.com>
2023-03-15 14:32:23 +0100

    feat: :safety_vest: Add validation and improve code performance
    Add validation to edit todo dialog and refactor updateTodo method so users can't add an existing todo to the list

--- src/app/pages/todos-wrapper/todo/edit-modal/todo-modal.component.ts
+++ src/app/pages/todos-wrapper/todo/edit-modal/todo-modal.component.ts
@@ -16,51 +16,80 @@ import { NotifierService } from '../../../../services/notifier.service';
   </div>
   <mat-divider></mat-divider>
 <div mat-dialog-content>
-      <form>
+      <form [formGroup]="editTodoForm">
       <mat-form-field class="example-full-width">
-        <input matInput [(ngModel)]="todoValue">
+        <mat-label> Enter Edited Todo </mat-label>
+        <input matInput placeholder="Update James Details" [(ngModel)]="todoValue"  formControlName="todo">
       </mat-form-field>
 
       <button mat-raised-button color="primary" (click)="updateTodo()">
         Update
       </button>
+      <div *ngIf="todo.errors && (todo.dirty || todo.touched)">
+          <span *ngIf="todo.errors.required">Todo Name is required</span>
+          <span *ngIf="todo.errors.minlength">Todo should be at least 10 characters long</span>
+        </div>
       </form>
 </div>
   `,
   styles: [
     `
     mat-form-field {
-  width: 70%;
-}
+      width: 70%;
+    }
 
-button:last-child {
-  margin-left: 10px;
-}
+    button:last-child {
+      margin-left: 10px;
+    }
 
+    span {
+      color: red;
+    }
     `
   ]
 })
 export class TodoModalComponent implements OnInit{
+  editTodoForm!: FormGroup;
+  todo!: FormControl;
   todoValue : string = '';
   taskObj : Todo = new Todo();
+  todoArr!: Todo[];
 
   constructor(@Inject(MAT_DIALOG_DATA) public todoData: Todo, private todoService : TodosService, private popupRef: MatDialogRef<TodoModalComponent>, private notifierService: NotifierService) {
   }
 
   ngOnInit(): void {
+    this.todo = new FormControl('', [Validators.required, Validators.minLength(10)]);
+    this.editTodoForm = new FormGroup({todo: this.todo})
     if (this.todoData) {
       this.todoValue = this.todoData.todo_name;
     }
   }
 
+  getAllTodo() {
+    this.todoService.getAllTodo().subscribe(
+      res => this.todoArr = res,
+      err => this.notifierService.showNotification('Something went wrong. could not fetch todos', 'ok', 'error')
+    )
+  }
+
   updateTodo() {
+  if (this.editTodoForm.valid) {
+    this.getAllTodo();
     this.taskObj.todo_name = this.todoValue;
     this.taskObj.id = this.todoData.id;
-    this.todoService.editTodo( this.taskObj).subscribe(res => {
-      this.notifierService.showNotification('Todo has been updated Successfully', 'ok', 'success')
-          this.popupRef.close('updated');
-    }, err=> {
-      this.notifierService.showNotification('Something went wrong, could not update todo pls try again', 'ok', 'error');
-    })
+    const todoExist = this.todoArr.find(todo => todo.todo_name === this.taskObj.todo_name);
+    if (!todoExist) {
+      this.todoService.editTodo( this.taskObj).subscribe(res => {
+        this.notifierService.showNotification('Todo has been updated Successfully', 'ok', 'success')
+            this.popupRef.close('updated');
+      }, err=> {
+        this.notifierService.showNotification('Something went wrong, could not update todo pls try again', 'ok', 'error');
+      })
+    }
+    else {
+      this.notifierService.showNotification('A todo with the edited name already exist in your list, pls enter a new name', 'ok', 'error');
+    }
+  }
   }
 }
