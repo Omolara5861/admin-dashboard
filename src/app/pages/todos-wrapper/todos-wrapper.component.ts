bc5de77a3695476e8a08e8e2bea00c3263e84e26
Omolara5861 <debbiegterra@gmail.com>
2023-03-15 01:01:28 +0100

    style: :lipstick: Update todo page UI by removing mat-card tag

--- src/app/pages/todos-wrapper/todos-wrapper.component.ts
+++ src/app/pages/todos-wrapper/todos-wrapper.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todos-wrapper',
   template: `
-  <mat-card style="padding: 20px">
     <app-todo></app-todo>
     <app-api-todos></app-api-todos>
-  </mat-card>
   `
 })
 export class TodosWrapperComponent {
